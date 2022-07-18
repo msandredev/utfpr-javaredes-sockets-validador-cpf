6ee1449b3ba82fbfefc00e7cc75e7e123e3d0a56
André Meira dos Santos <msandredev@gmail.com>
2022-07-18 00:03:26 -0300

    Adicionado codigo da atividade 01

---
+++ src/socket/validacpf/Cliente.java
@@ -0,0 +1,41 @@
+package socket.validacpf;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Cliente {
+    private static Socket socket;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+        try {
+            socket = new Socket("127.0.0.1", 50000);
+            entrada = new DataInputStream(socket.getInputStream());
+            saida = new DataOutputStream(socket.getOutputStream());
+
+            // Solicitado a digitação do CPF
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Digite o CPF: ");
+            String cpf = br.readLine();
+
+            // Saída do CPF digitado
+            saida.writeUTF(cpf);
+
+            // Resultado do servidor
+            String resultado = entrada.readUTF();
+
+            if (resultado.equals("true")) {
+                System.out.println("CPF valido!");
+            } else {
+                System.out.println("CPF invalido!");
+            }
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/socket/validacpf/Servidor.java
@@ -0,0 +1,79 @@
+package socket.validacpf;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Servidor {
+    private static Socket socket;
+    private static ServerSocket server;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+        try{
+            // Porta que será utilizada pelo ServerSocket
+            server = new ServerSocket(50000);
+            socket = server.accept();
+
+            // Fluxos de entrada/saída
+            entrada = new DataInputStream(socket.getInputStream());
+            saida = new DataOutputStream(socket.getOutputStream());
+
+            // Recebimento do valor inteiro
+            String cpf = entrada.readUTF();
+
+            // processamento do valor
+            System.out.println("CPF informado: " + cpf);
+
+            // envio dos dados (resultado)
+            saida.writeUTF(String.valueOf(isCpfValido(cpf)));
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isCpfValido(String cpf) {
+        String CPFRegex = "^([0-9]{3}\\.?){3}-?[0-9]{2}$";
+        Pattern p = Pattern.compile(CPFRegex);
+        Matcher m = p.matcher(cpf);
+
+        if (!m.matches()) {
+            System.out.println("CPF invalido!\nO numero de CPF deve conter 11 digitos (Ex: 111.222.333-00");
+            return false;
+        } else {
+            cpf = cpf.replace(".", "");
+            cpf = cpf.replace("-", "");
+
+            int n1, n2;
+            int dig1, dig2, resto, digCpf;
+            String resultDigit;
+
+            n1 = n2 = 0;
+            dig1 = dig2 = resto = 0;
+
+            for (int i = 1; i < cpf.length() - 1; i++) {
+                digCpf = Integer.valueOf(cpf.substring(i-1, i)).intValue();
+                n1 = n1 + (11 - i) * digCpf;
+                n2 = n2 + (12 - i) * digCpf;
+            };
+
+            resto = (n1 % 11);
+            dig1 = resto > 2 ? 11 - resto : 0;
+
+            n2 = n2 + 2 * dig1;
+
+            resto = (n2 % 11);
+            dig2 = resto > 2 ? 11 - resto : 0;
+
+            String verificador = cpf.substring(cpf.length() - 2, cpf.length());
+            String resultVerificador = String.valueOf(dig1) + String.valueOf(dig2);
+
+            return verificador.equals(resultVerificador);
+        }
+    }
+}
